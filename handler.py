46f3537bd1dc9bf40d99627142e2523f8b4a37dc
gavinwang <gavin.wang@silkrode.com.tw>
2020-06-23 14:49:22 +0800

    add code

--- handler.py
+++ handler.py
@@ -1,12 +1,13 @@
 from linebot.models import TextSendMessage, SendMessage, ImageSendMessage, VideoSendMessage, ImagemapSendMessage, \
-    BaseSize, URIImagemapAction, MessageImagemapAction, ImagemapArea
+    BaseSize, URIImagemapAction, MessageImagemapAction, ImagemapArea, TemplateSendMessage, ImageCarouselTemplate, \
+    ImageCarouselColumn,PostbackTemplateAction
 
-
+blackman_pic_url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdNlz5PjZDea1C5tI8p_Tx_mEs84KgCKrM_rJJVXdV9ZooNvo_KA&s'
 def line_reply_handler(message) -> SendMessage:
     if message == "來點圖片":
         return blackman_questions_photo()
     elif message == "多點圖片":
-        return more_lackman_questions_photo()
+        return more_blackman_questions_photo()
     else:
         return repeat(message)
 
@@ -18,8 +19,8 @@ def repeat(message) -> TextSendMessage:
 
 def blackman_questions_photo() -> ImageSendMessage:
     return ImageSendMessage(
-        original_content_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdNlz5PjZDea1C5tI8p_Tx_mEs84KgCKrM_rJJVXdV9ZooNvo_KA&s',
-        preview_image_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdNlz5PjZDea1C5tI8p_Tx_mEs84KgCKrM_rJJVXdV9ZooNvo_KA&s'
+        original_content_url=blackman_pic_url,
+        preview_image_url=blackman_pic_url
     )
 
 
@@ -30,16 +31,41 @@ def video() -> VideoSendMessage:
     )
 
 
-def more_lackman_questions_photo() -> ImagemapSendMessage:
-    connect_2_website = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdNlz5PjZDea1C5tI8p_Tx_mEs84KgCKrM_rJJVXdV9ZooNvo_KA&s'
+def more_blackman_questions_photo() -> TemplateSendMessage:
+    return TemplateSendMessage(
+    alt_text='blackman_pic_url',
+    template=ImageCarouselTemplate(
+        columns=[
+            ImageCarouselColumn(
+                image_url=blackman_pic_url,
+                action=PostbackTemplateAction(
+                    label='postback1',
+                    text='postback text1',
+                    data='action=buy&itemid=1'
+                )
+            ),
+            ImageCarouselColumn(
+                image_url=blackman_pic_url,
+                action=PostbackTemplateAction(
+                    label='postback2',
+                    text='postback text2',
+                    data='action=buy&itemid=2'
+                )
+            )
+        ]
+    )
+)
+
+def map_image() -> ImagemapSendMessage:
+    main_photo = blackman_pic_url
     preview_text = 'this is an imagemap'
     return ImagemapSendMessage(
-        base_url=connect_2_website,
+        base_url=main_photo,
         alt_text=preview_text,
         base_size=BaseSize(height=1040, width=1040),
         actions=[
             URIImagemapAction(
-                link_uri='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdNlz5PjZDea1C5tI8p_Tx_mEs84KgCKrM_rJJVXdV9ZooNvo_KA&s',
+                link_uri=blackman_pic_url,
                 area=ImagemapArea(
                     x=0, y=0, width=520, height=520
                 )
@@ -51,13 +77,13 @@ def more_lackman_questions_photo() -> ImagemapSendMessage:
                 )
             ),
             URIImagemapAction(
-                link_uri='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdNlz5PjZDea1C5tI8p_Tx_mEs84KgCKrM_rJJVXdV9ZooNvo_KA&s',
+                link_uri=blackman_pic_url,
                 area=ImagemapArea(
                     x=0, y=520, width=520, height=520
                 )
             ),
             URIImagemapAction(
-                link_uri='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdNlz5PjZDea1C5tI8p_Tx_mEs84KgCKrM_rJJVXdV9ZooNvo_KA&s',
+                link_uri=blackman_pic_url,
                 area=ImagemapArea(
                     x=520, y=520, width=520, height=520
                 )
