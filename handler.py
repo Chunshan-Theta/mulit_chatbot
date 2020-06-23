58f9cb7966ab3d6f922bcc00e211e2bda9e43bcb
gavinwang <gavin.wang@silkrode.com.tw>
2020-06-23 09:29:54 +0800

    add env

--- handler.py
+++ handler.py
@@ -1,12 +1,21 @@
-from linebot.models import TextSendMessage, SendMessage
+from linebot.models import TextSendMessage, SendMessage, ImageSendMessage
 
 
 def line_reply_handler(message) -> SendMessage:
-    reply_message = repeat(message)
-    return reply_message
+    if message == "來點圖片":
+        return random_photo()
+    else:
+        return repeat(message)
 
 
 def repeat(message) -> TextSendMessage:
     reply_message = TextSendMessage(text=message)
     return reply_message
 
+
+def random_photo() -> TextSendMessage:
+    reply_message = ImageSendMessage(
+        original_content_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdNlz5PjZDea1C5tI8p_Tx_mEs84KgCKrM_rJJVXdV9ZooNvo_KA&s',
+        preview_image_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdNlz5PjZDea1C5tI8p_Tx_mEs84KgCKrM_rJJVXdV9ZooNvo_KA&s'
+    )
+    return reply_message
