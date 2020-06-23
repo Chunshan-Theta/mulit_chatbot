317b0a3f096af696aa092d9f828090cb3dd3c932
gavinwang <gavin.wang@silkrode.com.tw>
2020-06-23 16:14:35 +0800

    add code

--- handler.py
+++ handler.py
@@ -3,37 +3,43 @@ from linebot.models import TextSendMessage, SendMessage, ImageSendMessage, Video
     ImageCarouselColumn, PostbackTemplateAction, ButtonsTemplate,MessageTemplateAction,URITemplateAction
 
 blackman_pic_url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdNlz5PjZDea1C5tI8p_Tx_mEs84KgCKrM_rJJVXdV9ZooNvo_KA&s'
+
 def line_reply_handler(message) -> SendMessage:
     if message == "來點圖片":
-        return blackman_questions_photo()
+        return send_a_picture()
     elif message == "多點圖片":
         return more_blackman_questions_photo()
     elif message == "選":
-        return list_question()
+        return choice_with_a_pictures()
     else:
         return repeat(message)
 
 
+# Text message
 def repeat(message) -> TextSendMessage:
     reply_message = TextSendMessage(text=message)
     return reply_message
 
 
-def blackman_questions_photo() -> ImageSendMessage:
+# Picture message
+def send_a_picture(original_content_url=blackman_pic_url, preview_image_url=blackman_pic_url) -> ImageSendMessage:
     return ImageSendMessage(
-        original_content_url=blackman_pic_url,
-        preview_image_url=blackman_pic_url
+        original_content_url=original_content_url,
+        preview_image_url=preview_image_url
     )
 
-
+"""
 def video() -> VideoSendMessage:
     return VideoSendMessage(
         original_content_url='https://example.com/original.mp4',
         preview_image_url='https://example.com/preview.jpg'
     )
+"""
 
 
-
+# Picture map
+# 單張圖，動態點擊區域
+# a single pic and multi-clicked area
 def map_image() -> ImagemapSendMessage:
     main_photo = blackman_pic_url
     preview_text = 'this is an imagemap'
@@ -69,17 +75,18 @@ def map_image() -> ImagemapSendMessage:
         ]
     )
 
-def list_question():
+
+def choice_with_a_pictures(title='Menu',text='Please select'):
     return TemplateSendMessage(
-    alt_text='Buttons template',
+    alt_text='we need your choice. please make your choice on you mobile',
     template=ButtonsTemplate(
         thumbnail_image_url=blackman_pic_url,
-        title='Menu',
-        text='Please select',
+        title=title,
+        text=text,
         actions=[
             PostbackTemplateAction(
                 label='postback',
-                text='clieked',
+                text=None,
                 data='action=buy&itemid=1'
             ),
             MessageTemplateAction(
